c1202065b8ff2d196cc7062ea561a96b6d38c470
grenndalf <b.olczak4@wp.pl>
2020-12-03 13:11:16 +0100

    update

--- src/main/java/App/App.java
+++ src/main/java/App/App.java
@@ -25,7 +25,6 @@ public class App extends Application {
 //        primaryStage.setResizable(false);
         primaryStage.setScene(scene);
         primaryStage.show();
-        System.out.println("dupa");
     }
 
     public FXMLLoader initFxml() {
