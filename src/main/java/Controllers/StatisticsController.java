00d238be2f1fab11813790860f649797e34e240f
grenndalf <b.olczak4@wp.pl>
2020-12-06 20:54:44 +0100

    chart added in statistics

--- src/main/java/Controllers/StatisticsController.java
+++ src/main/java/Controllers/StatisticsController.java
@@ -5,12 +5,15 @@ import RegularClasses.Mediator.ControllerHolder;
 import RegularClasses.Tables.GaugeMeasurement;
 import RegularClasses.Utils.Utils;
 import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
+import javafx.scene.control.Tooltip;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Arc;
 
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 
@@ -19,36 +22,75 @@ public class StatisticsController {
     public Arc dataArc;
     @FXML
     public Label correctDataInfo;
+    @FXML
+    public AnchorPane statisticsContainer;
 
 
     DbActions dbActions = new DbActions();
     List<GaugeMeasurement> selectedTownData = dbActions.queryForDataOfSelectedTown(ControllerHolder.getInstance().getTownName());
-    List<Double> correctedDataList = correctData(selectedTownData);
+    List<GaugeMeasurement> correctedDataList = correctData(selectedTownData);
 
     @FXML
     void initialize() {
         initDataArcValues();
         initCorrectDataInfo();
+        statisticsContainer.getChildren().add(getLineChart());
+    }
+
+    private LineChart<Number, Number> getLineChart() {
         NumberAxis xAxis = new NumberAxis();
         NumberAxis yAxis = new NumberAxis();
 
-        xAxis.setMinorTickVisible(false);
-        if (returnMaxOrMinYear(true) - returnMaxOrMinYear(false) > 50) {
-            xAxis.setAutoRanging(false);
+
+        xAxis.setAutoRanging(false);
+        xAxis.setTickUnit(1);
+        if (getYearRange().size() > 60) {
+            System.out.println(getYearRange().size());
             xAxis.setTickUnit(5);
         }
-        xAxis.setLowerBound(returnMaxOrMinYear(false)-1);
-        xAxis.setUpperBound(returnMaxOrMinYear(true)+1);
-        XYChart.Series series = new XYChart.Series();
-        series.getData().add(new XYChart.Data<>());
+        xAxis.setMinorTickVisible(false);
+        xAxis.setLowerBound(returnMaxOrMinYear(false) - 1);
+        xAxis.setUpperBound(returnMaxOrMinYear(true) + 2);
+
+        Map<Short, Double> dataMap = new HashMap<>();
+        XYChart.Series<Number, Number> series = new XYChart.Series<>();
+
+        getYearRange().forEach(aShort -> {
+                    double average = correctedDataList.stream()
+                            .filter(gaugeMeasurement -> gaugeMeasurement.getMeasurementYear() == aShort)
+                            .collect(Collectors.averagingDouble(value -> value.getData2().doubleValue()));
+                    if (average < 9999)
+                        dataMap.put(aShort, average);
+                });
+
+
+        LineChart<Number, Number> lineChart = new LineChart<>(xAxis, yAxis);
+        lineChart.setPrefWidth(1100);
+        lineChart.setPrefHeight(400);
+        lineChart.getData().add(series);
+        lineChart.setLegendVisible(false);
+        dataMap.forEach((aShort, aDouble) -> series.getData().add(new XYChart.Data<>(aShort,aDouble)));
+        for (XYChart.Data<Number, Number> entry : series.getData()) {
+            Tooltip t = new Tooltip(String.format("%.2f",entry.getYValue().doubleValue()) + "\n" + entry.getXValue());
+            Tooltip.install(entry.getNode(), t);
+        }
+        return lineChart;
+    }
+
+    private SortedSet<Short> getYearRange() {
+        return correctedDataList.stream().map(GaugeMeasurement::getMeasurementYear)
+                .sorted(Comparator.naturalOrder())
+                .collect(Collectors.toCollection(TreeSet::new));
     }
 
     private Short returnMaxOrMinYear(Boolean returnMaxYear) {
-        if (selectedTownData.size() < 1) return 0;
+        if (getYearRange().size() < 1) return 0;
         else {
-            return returnMaxYear ?
-                    selectedTownData.stream().map(GaugeMeasurement::getMeasurementYear).max(Short::compareTo).get()
-                    : selectedTownData.stream().map(GaugeMeasurement::getMeasurementYear).min(Short::compareTo).get();
+            // dodac try/catch zawierajacy obsluge nosuchelementexeption
+            if (returnMaxYear) {
+                return selectedTownData.stream().filter(gaugeMeasurement -> gaugeMeasurement.getMeasurementYear() != 2019).map(GaugeMeasurement::getMeasurementYear).max(Short::compareTo).get();
+            }
+            return selectedTownData.stream().map(GaugeMeasurement::getMeasurementYear).min(Short::compareTo).get();
         }
     }
 
@@ -61,11 +103,10 @@ public class StatisticsController {
         dataArc.setStartAngle(450 - Utils.divideIntegers(correctedDataList.size(), selectedTownData.size()) * 360);
     }
 
-    private List<Double> correctData(List<GaugeMeasurement> dataToCorrect) {
+    private List<GaugeMeasurement> correctData(List<GaugeMeasurement> dataToCorrect) {
         return dataToCorrect
                 .stream()
-                .map(gaugeMeasurement -> gaugeMeasurement.getData2().doubleValue())
-                .filter(data -> data < 9999)
+                .filter(data -> data.getData2().doubleValue() < 9999)
                 .collect(Collectors.toList());
     }
 
